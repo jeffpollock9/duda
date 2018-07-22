d388d54c9d90a173ae3083aa24d4f5e50c9d072e
jeffpollock9 <jeffrey.pollock@smartodds.co.uk>
2018-07-22 18:40:50 +0100

    test move, seems ok

--- benchmark/gemm.cpp
+++ benchmark/gemm.cpp
@@ -17,13 +17,13 @@ static void BM_host_gemm(benchmark::State& state)
     const T alpha = 0.001;
     const T beta  = 0.02;
 
-    host_matrix<T> A = host_matrix<T>::Random(n, n);
-    host_matrix<T> B = host_matrix<T>::Random(n, n);
-    host_matrix<T> C = host_matrix<T>::Random(n, n);
+    host_matrix<T> a = host_matrix<T>::Random(n, n);
+    host_matrix<T> b = host_matrix<T>::Random(n, n);
+    host_matrix<T> c = host_matrix<T>::Random(n, n);
 
     for (auto _ : state)
     {
-        C = alpha * A * B + beta * C;
+        c = alpha * a * b + beta * c;
     }
 }
 
@@ -34,13 +34,13 @@ static void BM_device_gemm(benchmark::State& state)
     const T alpha = 0.001;
     const T beta  = 0.02;
 
-    device_matrix<T> A = device_matrix<T>::random_uniform(n, n);
-    device_matrix<T> B = device_matrix<T>::random_uniform(n, n);
-    device_matrix<T> C = device_matrix<T>::random_uniform(n, n);
+    device_matrix<T> a = device_matrix<T>::random_uniform(n, n);
+    device_matrix<T> b = device_matrix<T>::random_uniform(n, n);
+    device_matrix<T> c = device_matrix<T>::random_uniform(n, n);
 
     for (auto _ : state)
     {
-        gemm(duda::op::none, duda::op::none, alpha, A, B, beta, C);
+        gemm(duda::op::none, duda::op::none, alpha, a, b, beta, c);
     }
 }
 
--- test/device_matrix.cpp
+++ test/device_matrix.cpp
@@ -29,17 +29,10 @@ device_matrix<T> copy(const host_matrix<T>& host)
     return {host.data(), rows, cols};
 }
 
-TEST_CASE("default_ctor", "[device_matrix]")
+TEST_CASE("default ctor", "[device_matrix]")
 {
-    SECTION("using float")
-    {
-        device_matrix<float> x;
-    }
-
-    SECTION("using double")
-    {
-        device_matrix<double> x;
-    }
+    device_matrix<float> x;
+    device_matrix<double> y;
 }
 
 template <typename T>
@@ -47,26 +40,45 @@ void test_copy_ctor(const int rows, const int cols)
 {
     device_matrix<T> x_d = device_matrix<T>::random_normal(rows, cols);
     device_matrix<T> y_d(x_d);
+    device_matrix<T> z_d = x_d;
 
     REQUIRE(x_d.data() != y_d.data());
+    REQUIRE(x_d.data() != z_d.data());
 
     host_matrix<T> x_h = copy(x_d);
     host_matrix<T> y_h = copy(y_d);
+    host_matrix<T> z_h = copy(z_d);
 
     REQUIRE(x_h.isApprox(y_h));
+    REQUIRE(x_h.isApprox(z_h));
 }
 
-TEST_CASE("copy_ctor", "[device_matrix]")
+TEST_CASE("copy ctor", "[device_matrix]")
 {
-    SECTION("using float")
-    {
-        test_copy_ctor<float>(16, 16);
-    }
+    test_copy_ctor<float>(16, 16);
+    test_copy_ctor<double>(32, 32);
+}
 
-    SECTION("using double")
-    {
-        test_copy_ctor<double>(32, 32);
-    }
+template <typename T>
+void test_move_ctor(const int rows, const int cols)
+{
+    device_matrix<T> x_d = device_matrix<T>::random_normal(rows, cols);
+    host_matrix<T> x_h   = copy(x_d);
+
+    device_matrix<T> y_d = std::move(x_d);
+    host_matrix<T> y_h   = copy(y_d);
+
+    REQUIRE(x_d.data() == nullptr);
+
+    REQUIRE(x_h.isApprox(y_h));
+    REQUIRE(y_d.rows() == rows);
+    REQUIRE(y_d.cols() == cols);
+}
+
+TEST_CASE("move ctor", "[device_matrix]")
+{
+    test_move_ctor<float>(16, 16);
+    test_move_ctor<double>(32, 32);
 }
 
 template <typename T>
@@ -81,15 +93,8 @@ void test_transfer(const int rows, const int cols)
 
 TEST_CASE("host -> device -> host", "[device_matrix]")
 {
-    SECTION("using float")
-    {
-        test_transfer<float>(4, 5);
-    }
-
-    SECTION("using double")
-    {
-        test_transfer<double>(10, 1);
-    }
+    test_transfer<float>(4, 5);
+    test_transfer<double>(10, 1);
 }
 
 template <typename T>
@@ -101,7 +106,7 @@ void test_axpy(const T alpha, const int rows, const int cols)
     host_matrix<T> x_h = copy(x_d);
     host_matrix<T> y_h = copy(y_d);
 
-    duda::axpy(alpha, x_d, y_d);
+    axpy(alpha, x_d, y_d);
     y_h = alpha * x_h + y_h;
 
     REQUIRE(y_h.isApprox(copy(y_d)));
@@ -109,29 +114,22 @@ void test_axpy(const T alpha, const int rows, const int cols)
 
 TEST_CASE("axpy", "[device_matrix][blas]")
 {
-    SECTION("using float")
-    {
-        test_axpy<float>(3.14, 5, 6);
-    }
-
-    SECTION("using double")
-    {
-        test_axpy<double>(-3.14, 51, 16);
-    }
+    test_axpy<float>(3.14, 5, 6);
+    test_axpy<double>(-3.14, 51, 16);
 }
 
 template <typename T>
-void test_gemm(const T alpha, const T beta, const int rows, const int cols)
+void test_gemm(const T alpha, const T beta, const int n)
 {
-    auto A_d = device_matrix<T>::random_normal(rows, cols);
-    auto B_d = device_matrix<T>::random_normal(rows, cols);
-    auto C_d = device_matrix<T>::random_normal(rows, cols);
+    auto A_d = device_matrix<T>::random_normal(n, n);
+    auto B_d = device_matrix<T>::random_normal(n, n);
+    auto C_d = device_matrix<T>::random_normal(n, n);
 
     host_matrix<T> A_h = copy(A_d);
     host_matrix<T> B_h = copy(B_d);
     host_matrix<T> C_h = copy(C_d);
 
-    duda::gemm(duda::op::none, duda::op::none, alpha, A_d, B_d, beta, C_d);
+    gemm(duda::op::none, duda::op::none, alpha, A_d, B_d, beta, C_d);
     C_h = alpha * A_h * B_h + beta * C_h;
 
     REQUIRE(C_h.isApprox(copy(C_d)));
@@ -139,13 +137,29 @@ void test_gemm(const T alpha, const T beta, const int rows, const int cols)
 
 TEST_CASE("gemm", "[device_matrix][blas]")
 {
-    SECTION("using float")
-    {
-        test_gemm<float>(0.1, 0.7, 10, 10);
-    }
+    test_gemm<float>(0.1, 0.7, 16);
+    test_gemm<double>(7, -0.7, 32);
+}
 
-    SECTION("using double")
-    {
-        test_gemm<double>(7, -0.7, 100, 2);
-    }
+template <typename T>
+void test_gemm_transpose(const T alpha, const T beta, const int n)
+{
+    auto A_d = device_matrix<T>::random_normal(n, n);
+    auto B_d = device_matrix<T>::random_normal(n, n);
+    auto C_d = device_matrix<T>::random_normal(n, n);
+
+    host_matrix<T> A_h = copy(A_d);
+    host_matrix<T> B_h = copy(B_d);
+    host_matrix<T> C_h = copy(C_d);
+
+    gemm(duda::op::transpose, duda::op::transpose, alpha, A_d, B_d, beta, C_d);
+    C_h = alpha * A_h.transpose() * B_h.transpose() + beta * C_h;
+
+    REQUIRE(C_h.isApprox(copy(C_d)));
+}
+
+TEST_CASE("gemm transpose ops", "[device_matrix][blas]")
+{
+    test_gemm_transpose<float>(0.1, 0.7, 512);
+    test_gemm_transpose<double>(7, -0.7, 256);
 }
