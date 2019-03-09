3b9a233ba96b4bb5c5322adc1d93cd0be1f73c19
jeff <jeffpollock9@gmail.com>
2019-03-09 11:58:03 +0000

    fix pyduda

--- python/src/pyduda.cpp
+++ python/src/pyduda.cpp
@@ -6,7 +6,7 @@ void init_device_matrix(py::module&);
 void init_device_vector(py::module&);
 void init_random(py::module&);
 
-PYBIND11_MODULE(example, m)
+PYBIND11_MODULE(pyduda, m)
 {
     init_device_matrix(m);
     init_device_vector(m);
